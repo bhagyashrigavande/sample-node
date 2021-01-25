1a3a991f440e16e03ac1b8b60ec4d457dc5f4a55
Bhagyashri <bhagyshreemahale12@gmail.com>
2021-01-25 14:50:49 +0800

    added default route to application

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 
 app.use('/index', (req, res, next) => {
     res.send('Hello from sample app');
-})
+});
 
 app.use('/test', (req, res, next) => {
     res.send('this is test');
-})
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<p> Welcome to Simple node JS application </p>');
+});
 
 app.listen(3000, () => {
     console.log('app is working on port 3000')
